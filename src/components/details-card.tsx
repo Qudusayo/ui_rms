2ad4a5058eb42d06ace50eeb026649668cce62c0
Ayoola Abdulqudus <qqudusayo@gmail.com>
2025-02-27 12:24:52 +0100

    updated details card

--- src/components/details-card.tsx
+++ src/components/details-card.tsx
@@ -1,7 +1,24 @@
-import React from "react";
+import styled from "styled-components";
+
+const Card = styled.div`
+  background-color: #f9f9f9;
+  border: 1px solid red;
+`;
+
+const Title = styled.span``;
+
+const Value = styled.span`
+  display: block;
+  font-size: xx-large;
+`;
 
 const DetailsCard = () => {
-  return <div>DetailsCard</div>;
+  return (
+    <Card>
+      <Title>Department</Title>
+      <Value>Computer Science</Value>
+    </Card>
+  );
 };
 
 export default DetailsCard;
