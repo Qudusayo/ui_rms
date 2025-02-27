856110f41632cb4fa828ebe6b2e43b33d4988970
Ayoola Abdulqudus <qqudusayo@gmail.com>
2025-02-27 12:37:53 +0100

    completed details card

--- src/components/details-card.tsx
+++ src/components/details-card.tsx
@@ -2,21 +2,32 @@ import styled from "styled-components";
 
 const Card = styled.div`
   background-color: #f9f9f9;
-  border: 1px solid red;
+  border-radius: 8px;
+  padding: 24px;
 `;
 
-const Title = styled.span``;
+const Title = styled.span`
+  color: #484649;
+  font-size: 12px;
+`;
 
 const Value = styled.span`
   display: block;
-  font-size: xx-large;
+  font-size: 22px;
+  color: #2856c3;
+  margin-top: 8px;
 `;
 
-const DetailsCard = () => {
+type DetailsCardProps = {
+  title: string;
+  value: string;
+};
+
+const DetailsCard = ({ title, value }: DetailsCardProps) => {
   return (
     <Card>
-      <Title>Department</Title>
-      <Value>Computer Science</Value>
+      <Title>{title}</Title>
+      <Value>{value}</Value>
     </Card>
   );
 };
