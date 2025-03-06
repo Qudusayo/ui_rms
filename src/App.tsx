b28d4fb7bcba6c156653865ee5e8764372aaccd0
Ayoola Abdulqudus <qqudusayo@gmail.com>
2025-03-06 14:48:11 +0100

    setup results page

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router";
 import { LoginPage } from "./pages/login-page";
 import { PasswordResetPage } from "./pages/password-reset";
 import { ConfirmEmailPage } from "./pages/confirm-email";
+import Results from "./pages/results";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Route index element={<LoginPage />} />
       <Route path="password-reset" element={<PasswordResetPage />} />
       <Route path="confirm-email" element={<ConfirmEmailPage />} />
+      <Route path="results" element={<Results />} />
     </Routes>
   );
 }
