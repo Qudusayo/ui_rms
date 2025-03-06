fc29f7d2eabf4852a677631629c06d7632a5e05a
Ayoola Abdulqudus <qqudusayo@gmail.com>
2025-03-06 14:40:28 +0100

    Add DashboardPage component and route

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router";
 import { LoginPage } from "./pages/login-page";
 import { PasswordResetPage } from "./pages/password-reset";
 import { ConfirmEmailPage } from "./pages/confirm-email";
+import { DashboardPage } from "./pages/dashboard";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Route index element={<LoginPage />} />
       <Route path="password-reset" element={<PasswordResetPage />} />
       <Route path="confirm-email" element={<ConfirmEmailPage />} />
+      <Route path="dashboard" element={<DashboardPage />} />
     </Routes>
   );
 }
