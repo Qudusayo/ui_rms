52717ef122f8d4d61f06556ab5d595c89aaee262
Ridwan <adebayoridwan2k20@gmail.com>
2025-03-13 09:25:33 -0400

    Merge main into Login_page

--- src/pages/login-page.tsx
+++ src/pages/login-page.tsx
@@ -13,7 +13,7 @@ const FormContainer = styled.section`
 const H1 = styled.h1`
   font: Proxima Nova;
   font-size: 2.3rem;
-  font-weight: 600; 
+  font-weight: 600;
 `;
 
 const Pg = styled.p`
@@ -43,28 +43,29 @@ const CheckboxContainer = styled.div`
 
 const ButtonWrapper = styled.div`
   margin: 20px auto;
-`
+`;
 
 export const LoginPage = () => {
-  return <FormContainer>
-    <H1>University of Ibadan Result Management System</H1>
-    <Pg>Input your matric number and your student’s portal’s password to access your results</Pg>
-    <form action="#">
-      <InputSide>
-        <InputContainer name="Matric Number" placeholder="Input your Matric number here" />
-        <InputContainer name="Password" placeholder="Input your password" />
-      </InputSide>
-      <ButtonWrapper>
-        <Button name="Login" handleClick={() => console.log("Logging in")} />
-      </ButtonWrapper>
-      <FormFooter>
-        <CheckboxContainer>
-          <input type="checkbox" />
-          <span>Remind me</span>
-        </CheckboxContainer>
-        <Link to="/password-reset">Forgot password?</Link>
-      </FormFooter>
-    </form>
-
-  </FormContainer>;
+  return (
+    <FormContainer>
+      <H1>University of Ibadan Result Management System</H1>
+      <Pg>Input your matric number and your student’s portal’s password to access your results</Pg>
+      <form action="#">
+        <InputSide>
+          <InputContainer name="Matric Number" placeholder="Input your Matric number here" />
+          <InputContainer name="Password" placeholder="Input your password" />
+        </InputSide>
+        <ButtonWrapper>
+          <Button name="Login" handleClick={() => console.log("Logging in")} />
+        </ButtonWrapper>
+        <FormFooter>
+          <CheckboxContainer>
+            <input type="checkbox" />
+            <span>Remind me</span>
+          </CheckboxContainer>
+          <Link to="/password-reset">Forgot password?</Link>
+        </FormFooter>
+      </form>
+    </FormContainer>
+  );
 };
