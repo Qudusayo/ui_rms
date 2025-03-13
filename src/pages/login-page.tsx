afc86b09d1659ba7f026d5a1fc2a84689ac1a5b5
Ridwan <adebayoridwan2k20@gmail.com>
2025-03-13 06:25:07 -0400

    Login_page

--- src/pages/login-page.tsx
+++ src/pages/login-page.tsx
@@ -1,3 +1,44 @@
+import styled from "styled-components";
+import InputContainer from "../components/input";
+import { Link } from "react-router";
+import Button from "../components/Button";
+
+const FormContainer = styled.section`
+  width: 500px;
+  margin: 0px auto;
+  text-align: center;
+`;
+
+const H1 = styled.h1`
+  font: Proxima Nova;
+  font-size: 2.3rem;
+  font-weight: 600; 
+`;
+
+const Pg = styled.p`
+  font-size: 1.05rem;
+  letter-spacing: 0.25%;
+  padding: 0 100px;
+  font-weight: 400;
+`;
+
+const InputSide = styled.div`
+  margin-bottom: 40px;
+`;
+
 export const LoginPage = () => {
-  return <div>LoginPage</div>;
+  return <FormContainer>
+    <H1>University of Ibadan Result Management System</H1>
+    <Pg>Input your matric number and your student’s portal’s password to access your results</Pg>
+    <form action="#">
+      <InputSide>
+      <InputContainer name="Matric Number" placeholder="Input your Matric number here" />
+      <InputContainer name="Password" placeholder="Input your password" />
+      </InputSide>
+      <Button name="Login"/>
+      <input type="checkbox" /> <span>Remind me</span>
+      <Link to="/password-reset" style={{ marginLeft: "50%" }}>Forgot password?</Link>
+    </form>
+
+  </FormContainer>;
 };
