f54bd50c2f5b75c336ede473768da128a74e91fa
mujahidah <abdurrahmonmujahidah@gmail.com>
2025-03-20 11:40:36 +0100

    select

--- src/components/select.tsx
+++ src/components/select.tsx
@@ -14,9 +14,9 @@ const Select = styled.select`
     -moz-appearance: none;
      background-image: url("data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTYiIGhlaWdodD0iOSIgdmlld0JveD0iMCAwIDE2IDkiIGZpbGw9Im5vbmUiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+CjxwYXRoIGQ9Ik0wLjI5Mjg5MyAwLjI5Mjg5M0MwLjY4MzQxNyAtMC4wOTc2MzExIDEuMzE2NTggLTAuMDk3NjMxMSAxLjcwNzExIDAuMjkyODkzTDggNi41ODU3OUwxNC4yOTI5IDAuMjkyODkzQzE0LjY4MzQgLTAuMDk3NjMxMSAxNS4zMTY2IC0wLjA5NzYzMTEgMTUuNzA3MSAwLjI5Mjg5M0MxNi4wOTc2IDAuNjgzNDE3IDE2LjA5NzYgMS4zMTY1OCAxNS43MDcxIDEuNzA3MTFMOC43MDcxMSA4LjcwNzExQzguMzE2NTggOS4wOTc2MyA3LjY4MzQyIDkuMDk3NjMgNy4yOTI4OSA4LjcwNzExTDAuMjkyODkzIDEuNzA3MTFDLTAuMDk3NjMxMSAxLjMxNjU4IC0wLjA5NzYzMTEgMC42ODM0MTcgMC4yOTI4OTMgMC4yOTI4OTNaIiBmaWxsPSIjOTM5MDk0Ii8+Cjwvc3ZnPgo=");
      background-repeat: no-repeat;
-     background-position-x: 97.9%;
+     background-position-x: calc(100% - 10px);
      background-position-y: 50%;
-     background-size: 2%;
+     background-size: 14px;
      margin: 0.3em auto 2em;
      outline: none;
      padding-left : 10px;
