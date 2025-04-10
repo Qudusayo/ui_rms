f7d08c3f5c9546d3db036d2dee3913f1dd82f1c1
unknown <abdulrahmanabdulazeez34@gmail.com>
2025-04-10 11:18:51 +0100

    Results page created

--- src/pages/results.tsx
+++ src/pages/results.tsx
@@ -1,9 +1,26 @@
 import DashboardLayout from "../layouts/dashboard-layout";
+import TableComponent from "../components/table";
+import styled from "styled-components";
+import StudentDetails from "../components/student-details";
+import SelectComponent from "../components/select";
 
+const ResultWrapper = styled.div``;
+const Div = styled.div``;
 const Results = () => {
   return (
     <DashboardLayout>
-      <div>Results</div>
+      <ResultWrapper>
+        <StudentDetails
+          name="Azeez Abdulrahman A."
+          matric="250950"
+          department="Civil Engineering"
+          faculty="Technology"
+        />
+        <Div>
+          <SelectComponent />
+        </Div>
+        <TableComponent />
+      </ResultWrapper>
     </DashboardLayout>
   );
 };
